69c80479e36868a005c4c529ac5c4d9a198df459
YMB06 <youssefmf240@gmail.com>
2025-04-13 14:25:50 +0200

    Punto 4 Youssef Miftah Creamos una interfaz y le añadimos campos para luego añadirlos en el componente Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {HousingLocationComponent} from '../housing-location/housing-location.component';
+import {HousingLocation} from '../housinglocation';
 @Component({
   selector: 'app-home',
-  imports: [HousingLocationComponent],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
-   <section>
+    <section>
       <form>
         <input type="text" placeholder="Filter by city" />
         <button class="primary" type="button">Search</button>
@@ -14,8 +16,18 @@ import {HousingLocationComponent} from '../housing-location/housing-location.com
       <app-housing-location></app-housing-location>
     </section>
   `,
-  styleUrls: ['home.component.css'],
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
